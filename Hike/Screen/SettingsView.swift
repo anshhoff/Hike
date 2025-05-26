74f12c47c6fb153dc6394529ea4a215ac4588596
anshhoff <anshhoff@github.com>
2025-05-26 08:43:45 +0530

    Added the Settings View

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -1,18 +1,18 @@
 //
-//  CardView.swift
+//  SettingsView.swift
 //  Hike
 //
-//  Created by Ansh Hardaha on 27/04/25.
+//  Created by Ansh Hardaha on 26/05/25.
 //
 
 import SwiftUI
 
-struct CardView: View {
+struct SettingsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CardView()
+    SettingsView()
 }
---
+++ New Group/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ansh Hardaha on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
